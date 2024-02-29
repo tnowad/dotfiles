d457af53b168ebaf06edf0d6de0e2399b166bd82
Nguyen Minh Tuan <tnowad@gmail.com>
2024-03-01 02:17:24 +0700

    feat: remove unused config

--- apps/fish/.config/fish/config.fish
+++ apps/fish/.config/fish/config.fish
@@ -8,28 +8,10 @@ if status is-interactive
     set -g fish_greeting
 end
 
-
-# Jenv
-eval set -gx PATH '/home/tnowad/.jenv/shims' $PATH
-set -gx JENV_SHELL fish
-set -gx JENV_LOADED 1
-set -e JAVA_HOME
-set -e JDK_HOME
-jenv rehash 2>/dev/null
-jenv refresh-plugins
-function jenv
-    set command $argv[1]
-    set -e argv[1]
-
-    switch "$command"
-        case enable-plugin rehash shell shell-options
-            jenv "sh-$command" $argv | source
-        case '*'
-            command jenv "$command" $argv
-    end
+if type -q node
+    nvm use default >/dev/null &
 end
 
-# PNPM setup
 if not type -q pnpm
     npm install -g pnpm
     pnpm setup
@@ -40,5 +22,7 @@ set -gx PNPM_HOME "/home/tnowad/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
+
+export EDITOR="nvim"
 
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
