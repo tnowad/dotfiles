cf04971453e5e73601aa646109c0a8589eb7d25a
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-08 20:58:14 +0700

    chore: make remove greeting default in fish config

--- apps/fish/.config/fish/config.fish
+++ apps/fish/.config/fish/config.fish
@@ -1,11 +1,13 @@
+set -g fish_greeting
+
+
+if status is-interactive
+end
+
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c "cat ~/.config/fish/fish_plugins | fisher install"
-end
-
-if status is-interactive
-    set -g fish_greeting
 end
 
 if type -q node
@@ -32,3 +34,4 @@ export EDITOR="nvim"
 source "$HOME/.cargo/env.fish"
 
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
